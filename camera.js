917ca0d875f002112f8c79a23a30d7cc2eea1dcd
Daniel Dupont <ddupont.ext@louvre-hotels.com>
2019-04-23 22:53:42 +0200

    update

--- camera.js
+++ camera.js
@@ -2,7 +2,7 @@ const { spawn } = require('child_process');
 const express = require('express');
 const app = express();
 
-const vstream = spawn('raspivid', ['-t', '5000', '-o', 'vid.h264']);
+const vstream = spawn('raspivid', ['-t', '5000', '-o', 'vid.h264', '-n']);
 
 app.get('/stream', (req, res) => {
   vstream.stdout.pipe(res);
