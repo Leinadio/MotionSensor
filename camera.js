09d786fa65b3a30e6c7d373a3ff18dbbf496f155
Daniel Dupont <ddupont.ext@louvre-hotels.com>
2019-06-09 01:09:36 +0200

    update

--- camera.js
+++ camera.js
@@ -29,5 +29,5 @@ app.get('/', function(req, res) {
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listenidsdfdfng on port 3000!')
 });
