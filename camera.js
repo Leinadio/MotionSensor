1b8817133892e7f31618a57accc0efb4f88f79f6
Daniel Dupont <ddupont.ext@louvre-hotels.com>
2019-06-10 00:19:09 +0200

    update

--- camera.js
+++ camera.js
@@ -13,8 +13,8 @@ const app = express();
  * -fps Specify the frames per second to record
  * @type {ChildProcessWithoutNullStreams}
  */
-const child1 = spawn('raspivid', ['-hf', '-w', '1280', '-h', '1024', '-t', '5000', '-fps', '20', '-b', '5000000', '-o', 'pivideo.h264']);
-const child2 = spawn('MP4Box', ['-add', 'pivideo.h264', 'pivideo.mp4']);
+const child1 = spawn('raspivid', ['-hf', '-w', '1280', '-h', '1024', '-t', '5000', '-fps', '20', '-b', '5000000', '-o', '-', '|', 'MP4Box', '-add', 'pivideo.h264', 'pivideo.mp4']);
+// const child2 = spawn('MP4Box', ['-add', 'pivideo.h264', 'pivideo.mp4']);
 console.log('child2 : ', child2);
 
 app.get('/', function(req, res) {
