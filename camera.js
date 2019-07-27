dce2c966334ce8d503be53f7fb08b8a4310daebe
Daniel Dupont <danieldupont.contact@gmail.com>
2019-07-27 18:35:27 +0200

    update

--- camera.js
+++ camera.js
@@ -22,6 +22,6 @@ app.get('/', (req, res) => {
   child.stdout.pipe(res)
 });
 
-server.listen(3000, function () {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
