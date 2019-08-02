1ee6abb8a28da01114acfaf99935593d3868c967
Daniel Dupont <danieldupont.contact@gmail.com>
2019-08-03 01:31:31 +0200

    lol

--- camera.js
+++ camera.js
@@ -15,7 +15,8 @@ const app = express();
  * -o Set output
  * @type {ChildProcessWithoutNullStreams}
  */
-spawn('raspivid', ['-hf', '-w', '1280', '-h', '1024', '-t', '0', '-fps', '60', '-o', 'vid.mp4']);
+spawn('raspivid', ['-hf', '-w', '1280', '-h', '1024', '-t', '0', '-fps', '60', '-o', 'pivideo.h264']);
+spawn('MP4Box', ['-add', 'pivideo.h264', 'vid.mp4']);
 // const child = spawn('raspivid', ['-t', '9999999', '-o', '-', '-n']);
 
 // const videoStream = raspividStream({
