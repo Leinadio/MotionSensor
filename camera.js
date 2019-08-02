9e6454b3f1d63a08b3ce81f42af02d65f062fb5a
Daniel Dupont <danieldupont.contact@gmail.com>
2019-08-02 23:43:44 +0200

    lol

--- camera.js
+++ camera.js
@@ -16,6 +16,7 @@ const app = express();
  * @type {ChildProcessWithoutNullStreams}
  */
 const child = spawn('raspivid', ['-hf', '-w', '1280', '-h', '1024', '-t', '0', '-fps', '60']);
+console.log('child : ', child);
 // const child = spawn('raspivid', ['-t', '9999999', '-o', '-', '-n']);
 
 app.get('/', (req, res) => {
