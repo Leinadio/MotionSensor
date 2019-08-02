3a098037a551db17fe1bd29af23803531ed986c5
Daniel Dupont <danieldupont.contact@gmail.com>
2019-08-03 00:08:18 +0200

    lol

--- camera.js
+++ camera.js
@@ -1,9 +1,10 @@
 const { spawn } = require('child_process');
 const express = require('express');
+const raspividStream = require('raspivid-stream');
 const http = require("http");
 const fs = require('fs');
-const raspividStream = require('raspivid-stream');
 const app = express();
+const wss = require('express-ws')(app);
 
 /**
  * -n = no preview
@@ -19,16 +20,35 @@ const app = express();
 // const child = spawn('raspivid', ['-hf', '-w', '1280', '-h', '1024', '-t', '0', '-fps', '60']);
 // const child = spawn('raspivid', ['-t', '9999999', '-o', '-', '-n']);
 
-const videoStream = raspividStream({
-  rotation: 180,
-  preview: true
-});
 
 app.get('/', (req, res) => {
   videoStream.on('data', (data) => {
     console.log('data : ', data);
     data.pipe(res)
   });
+});
+
+app.ws('/video-stream', (ws, req) => {
+  ws.send(JSON.stringify({
+    action: 'init',
+    width: '960',
+    height: '540'
+  }));
+
+  const videoStream = raspividStream({
+    rotation: 180,
+    preview: true
+  });
+
+  videoStream.on('data', (data) => {
+    ws.send(data, { binary: true }, (error) => { if (error) console.error(error); });
+  });
+
+  ws.on('close', () => {
+    console.log('Client left');
+    videoStream.removeAllListeners('data');
+  });
+
 });
 
 app.listen(3000, function () {
