5b881a1f4c56be05b4d16fec4d6b0abcb2e0298f
Daniel Dupont <danieldupont.contact@gmail.com>
2019-08-17 15:23:54 +0200

    dsdfd

--- camera.js
+++ camera.js
@@ -1,43 +1,28 @@
 const { spawn } = require('child_process');
 const express = require('express');
 const fs = require('fs');
-const raspividStream = require('raspivid-stream');
 const app = express();
+const Raspistill = require('node-raspistill').Raspistill;
+const camera = new Raspistill();
 
-/**
- * -n = no preview
- * -hf Set horizontal flip
- * -w set width
- * -h set height
- * -t set timeout
- * -b Set bitrate
- * -fps Specify the frames per second to record
- * -o Set output
- * @type {ChildProcessWithoutNullStreams}
- */
-spawn('raspivid', ['-hf', '-w', '1280', '-h', '1024', '-t', '0', '-fps', '60', '-o', 'pivideo.h264']);
-spawn('MP4Box', ['-add', 'pivideo.h264', 'vid.mp4']);
-// const child = spawn('raspivid', ['-t', '9999999', '-o', '-', '-n']);
-
-// const videoStream = raspividStream({
-//   rotation: 180,
-//   preview: true
-// });
-
-app.get('/', (req, res) => {
-  const a = fs.createReadStream('./pivideo.h264');
-  const head = {
-    'Content-Type': 'video/h264',
-  };
-  res.writeHead(200, head);
-  a.on('data', (data) => {
-    console.log('data : ', data)
-  })
-  a.pipe(res);
-  // videoStream.on('data', (data) => {
-  //   console.log('data : ', data.toString())
-  // });
+camera.takePhoto().then((photo) => {
+  console.log('photo : ', photo);
 });
+
+// app.get('/', (req, res) => {
+//   const a = fs.createReadStream('./pivideo.h264');
+//   const head = {
+//     'Content-Type': 'video/h264',
+//   };
+//   res.writeHead(200, head);
+//   a.on('data', (data) => {
+//     console.log('data : ', data)
+//   })
+//   a.pipe(res);
+//   // videoStream.on('data', (data) => {
+//   //   console.log('data : ', data.toString())
+//   // });
+// });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
