a64ceb5957ada73f6ef41c13c26e030f8c0b5940
Daniel Dupont <danieldupont.contact@gmail.com>
2019-08-26 23:54:16 +0200

    config: update

--- index.ts
+++ index.ts
@@ -5,13 +5,12 @@ import axios from 'axios';
 async function handleMotionValue({ status, description }: { status: number, description: string}) {
   if (status === 2) {
     const picture = await capturePicture();
-    console.log('picture : ', picture);
     const response = await axios({
       method: 'POST',
       url: 'http://192.168.1.43:8080/fileSend',
       data: picture.getBuffer(),
       headers: picture.getHeaders(),
-    })
+    });
     console.log('response : ', response);
   }
 }
