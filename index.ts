ae48bda9935a80479ded58fc08dab20fc03e7dde
Daniel Dupont <danieldupont.contact@gmail.com>
2019-08-26 23:46:49 +0200

    config: update capturePicture, remove his callback

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import Motion from './services/motion';
 import { capturePicture } from './services/camera'
 
-function handleMotionValue({ status, description }: { status: number, description: string}) {
+async function handleMotionValue({ status, description }: { status: number, description: string}) {
   if (status === 2) {
-    const picture = capturePicture();
+    const picture = await capturePicture();
     console.log('picture : ', picture);
   }
 
