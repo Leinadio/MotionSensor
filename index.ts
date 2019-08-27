6c2aa466241501b99044a72839ebb2872c806951
Daniel Dupont <danieldupont.contact@gmail.com>
2019-08-27 23:16:12 +0200

    config: (in progress) add authentication before handle sending file

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import Motion from './services/motion';
 import { capturePicture } from './services/camera'
+import { connectToApi } from "./services/authentication";
 import axios from 'axios';
 
 async function handleMotionValue({ status, description }: { status: number, description: string}) {
   if (status === 2) {
     const picture = await capturePicture();
+    const connected = connectToApi();
     const response = await axios({
       method: 'POST',
       url: 'http://192.168.1.43:8080/fileSend',
---
+++ services/authentication/index.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export async function connectToApi() {
+  const response = await axios({
+    method: 'POST',
+    url: 'https://dashbardleinadio.eu.auth0.com/oauth/token',
+    headers: { 'content-type': 'application/json' },
+    data: {
+      client_id:"YnMr32CjcdKUY7SFJryEnZlelzGP1pDa",
+      client_secret:"1p7y-q72RRdmhaC7ZDLshuALVfTPRKvWTqiQscj6PUP9EKAIjf5C7UjHcdFTcair",
+      audience:"home-automation",
+      grant_type:"client_credentials"
+    }
+  });
+  console.log('response : ', response);
+  return response;
+}
