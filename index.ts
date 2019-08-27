b4a2569fe84a43cd321cfa1896f22ac39761e26b
Daniel Dupont <danieldupont.contact@gmail.com>
2019-08-27 23:34:38 +0200

    config: (debug) add authentication before handle sending file

--- index.ts
+++ index.ts
@@ -3,17 +3,22 @@ import { capturePicture } from './services/camera'
 import { connectToApi } from "./services/authentication";
 import axios from 'axios';
 
+let connected: any = null;
+
 async function handleMotionValue({ status, description }: { status: number, description: string}) {
   if (status === 2) {
     const picture = await capturePicture();
-    const connected = connectToApi();
+    if (connected) {
+      connected = connectToApi();
+    }
+    console.log('connected : ', connected);
     const response = await axios({
       method: 'POST',
       url: 'http://192.168.1.43:8080/fileSend',
       data: picture.getBuffer(),
       headers: picture.getHeaders(),
     });
-    console.log('response : ', response);
+    // console.log('response : ', response);
   }
 }
 
