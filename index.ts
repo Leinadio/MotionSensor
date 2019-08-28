0b54d30c87fc227317c213cf1d0975a053c635e2
Daniel Dupont <danieldupont.contact@gmail.com>
2019-08-29 00:35:00 +0200

    update

--- index.ts
+++ index.ts
@@ -1,26 +1,42 @@
+import gql from 'graphql-tag';
+import axios from 'axios';
 import Motion from './services/motion';
 import { capturePicture } from './services/camera'
 import { connectToApi } from "./services/authentication";
-import axios from 'axios';
+import ApolloClient from 'apollo-boost';
 
-let connected: any = null;
+// let connected: any = null;
+
+const client = new ApolloClient({
+  uri: 'http://192.168.1.43:8080/'
+});
 
 async function handleMotionValue({ status, description }: { status: number, description: string}) {
   if (status === 2) {
     const picture = await capturePicture();
-    if (!connected) {
-      connected = await connectToApi();
-    }
-    console.log('connected : ', connected);
-    console.log('picture.getHeaders() :', picture.getHeaders());
-    // console.log('connected : ', connected);
-    const response = await axios({
-      method: 'POST',
-      url: 'http://192.168.1.43:8080/',
-      data: `{"query":"mutation {\\n  Image(id: 2, name: "lol"}){\\n    id,\\n    title\\n  }\\n}","variables":null}`,
-      // data: picture.getBuffer(),
-      headers: picture.getHeaders(),
-    });
+    // if (!connected) {
+    //   connected = await connectToApi();
+    // }
+    const UPLOAD_FILE = gql`
+      mutation singleUpload($file: Upload!) {
+        singleUpload(file: $file) {
+          filename
+        }
+      }
+    `;
+    client.mutate({
+      mutation: UPLOAD_FILE,
+      variables: { file: picture.getBuffer() },
+    })
+      .then(data => console.log('data : ', data))
+      .catch(error => console.error(error));    // console.log('connected : ', connected);
+    // const response = await axios({
+    //   method: 'POST',
+    //   url: 'http://192.168.1.43:8080/',
+    //   data: `{"query":"mutation {\\n  Image(id: 2, name: "lol"}){\\n    id,\\n    title\\n  }\\n}","variables":null}`,
+    //   // data: picture.getBuffer(),
+    //   headers: picture.getHeaders(),
+    // });
     // console.log('response : ', response);
   }
 }
