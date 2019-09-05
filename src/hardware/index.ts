46b9d4ab9d64e7be14b5181f60e92256b5a5871c
Daniel Dupont <danieldupont.contact@gmail.com>
2019-09-05 23:46:15 +0200

    feature/auth: (progress) test reorganize struture

--- src/hardware/index.ts
+++ src/hardware/index.ts
@@ -3,6 +3,7 @@ import Motion from './motion';
 console.log('Motion : ', Motion);
 
 export function start() {
+  console.log('ok')
   Board.on('ready', () => {
     console.log('The Board is ready');
     Motion();
