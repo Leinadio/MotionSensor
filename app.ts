35c1d6cce9e0b220be297f7db6abdc45cf872c2c
Daniel Dupont <danieldupont.contact@gmail.com>
2019-09-06 09:55:28 +0200

    feature/auth: (progress) test reorganize struture

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import { launchAuthentication } from './src/services/authentication';
 import Hardware from './src/hardware';
 
-export default function () {
+export default async function () {
   const { statusCode, data } = await launchAuthentication();
   if (statusCode === 401) {
     console.log(`Error Message : ${data.error} - ${data.result_description}`);
