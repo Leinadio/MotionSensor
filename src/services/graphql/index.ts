93da9b0458499f7cede0958fef76bf6fb606a073
Daniel Dupont <danieldupont.contact@gmail.com>
2019-09-06 17:17:40 +0200

    feature/auth: (progress) test reorganize struture

--- src/services/graphql/index.ts
+++ src/services/graphql/index.ts
@@ -1,6 +1,7 @@
 import ApolloClient from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloLink } from "apollo-link";
+import { setContext } from 'apollo-link-context';
 import { createUploadLink } from 'apollo-upload-client';
 import fetch from 'node-fetch';
 
@@ -9,7 +10,21 @@ const uploadLink = createUploadLink({
   fetch
 });
 
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  // const token = localStorage.getItem('token');
+  const token = '';
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: ApolloLink.from([uploadLink]),
+  // ølink: ApolloLink.from([uploadLink, authLink]),
+  link: uploadLink.concat(authLink),
 });
