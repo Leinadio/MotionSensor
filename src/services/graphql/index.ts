9fab608adb4cfd706927ed6caeb5d34a33c2fb60
Daniel Dupont <danieldupont.contact@gmail.com>
2019-09-07 08:28:41 +0200

    feature/auth: (progress) test reorganize struture

--- src/services/graphql/index.ts
+++ src/services/graphql/index.ts
@@ -27,6 +27,7 @@ const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
   // const token = localStorage.getItem('token');
   const token = '';
+  console.log('process.env.ACCESS_TOKEN : ', process.env.ACCESS_TOKEN);
   // return the headers to the context so httpLink can read them
   return {
     headers: {
